806b473f9bae03a199a61c51be66c7321185358b
Pa Thao <paiathao@gmail.com>
2024-08-19 20:30:25 -0500

    resubmission

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -67,10 +67,19 @@ while ( index < max) {
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
-
+let i = 0
+while (i<=5) {
+  console.log(i);
+  i++;
+}
 
 
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+let i = 10;
+while (i>=5) {
+  console.log(i);
+  i--;
+}
